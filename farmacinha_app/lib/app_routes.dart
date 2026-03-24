05323203186d6aa4bc0ef927fa06f4f0cbea2365
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-03-23 21:37:11 -0300

    feat(navigation): adiciona classe AppRoutes com rotas nomeadas

---
+++ farmacinha_app/lib/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String welcome = '/welcome';
+  static const String login = '/login';
+  static const String register = '/register';
+}
