68eccb86d764456a75c3f79977aef30fdc9785fe
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-04-07 15:53:53 -0300

    feat(auth): implementar validação de login e redirecionamento de cliente na LoginViewModel

--- farmacinha_app/lib/features/auth/view_models/login_view_model.dart
+++ farmacinha_app/lib/features/auth/view_models/login_view_model.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:farmacia_app/app/app_routes.dart';
+import 'package:farmacia_app/features/auth/data/mocks/mock_users.dart';
+import 'package:farmacia_app/features/auth/data/models/user_model.dart';
 
 class LoginViewModel extends ChangeNotifier {
   final TextEditingController emailController = TextEditingController();
@@ -20,12 +23,42 @@ class LoginViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> login() async {
+  Future<void> login(BuildContext context) async {
     final email = emailController.text;
     final password = passwordController.text;
-    
-    // Variável utilizada para evitar Warnings de POO
-    debugPrint("Tentativa de login - Usuário: $email | Senha: $password");
+
+    debugPrint("Tentativa de login - Usuário: $email");
+
+    try {
+      // Busca o usuário no Mock seguindo as credenciais
+      final user = MockUsers.getUsers().firstWhere(
+        (u) => u.email == email && u.password == password,
+      );
+
+      // Redirecionamento baseado no Role
+      // Apenas o cliente vai para a HomeClient por enquanto
+      if (user.role == UserRole.cliente) {
+        Navigator.pushReplacementNamed(context, AppRoutes.homeClient);
+      } else {
+        // Para os outros cargos, podemos exibir um alerta ou log enquanto as telas não existem
+        debugPrint("Usuário ${user.name} logado como ${user.role}. Tela em desenvolvimento.");
+        
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Acesso para ${user.role.name} ainda em desenvolvimento!"),
+            backgroundColor: Colors.orange,
+          ),
+        );
+      }
+    } catch (e) {
+      // Se o firstWhere não encontrar ninguém, ele lança um erro
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("E-mail ou senha inválidos!"),
+          backgroundColor: Colors.redAccent,
+        ),
+      );
+    }
   }
 
   @override
