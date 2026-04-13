a08d908a9bd0812df6de84cb39fb721fe9885174
Eduardo Lima <eduardolima.feob@gmail.com>
2026-04-13 19:38:54 -0300

    Feature Manager Create in Application And Update Mock Gerente

---
+++ farmacinha_app/lib/preview.dart
@@ -0,0 +1,11 @@
+import 'package:farmacia_app/features/manager/manager_shell_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ManagerShellScreen(), // ← troca aqui
+    ),
+  );
+}
